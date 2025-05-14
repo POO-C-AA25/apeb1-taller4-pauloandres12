926cfbcdab090f290ee237ab5e4882b56677f648
Paulo Bustamante <pbustamante@utpl.edu.ec>
2025-05-13 23:21:34 -0500

    actualizacion

--- Solucion_Codigo/Ejercicios_Taller4/src/ejercicios_taller4/Problema_3_Autobus.java
+++ Solucion_Codigo/Ejercicios_Taller4/src/ejercicios_taller4/Problema_3_Autobus.java
@@ -6,4 +6,23 @@ package ejercicios_taller4;
  */
 public class Problema_3_Autobus {
 
+    public String nombre;
+    public String grado;
+    public boolean permisoV;
+
+    public Problema_3_Autobus(String nombre, String grado, int permisoVigente) {
+        this.nombre = nombre;
+        this.grado = grado;
+        this.permisoV = (permisoVigente == 1);
+    }
+
+    public boolean permiso() {
+        return permisoV;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante: " + nombre
+                + "\nGrado: " + grado;
+    }
 }
--- Solucion_Codigo/Ejercicios_Taller4/src/ejercicios_taller4/Problema_3_Ejecutor.java
+++ Solucion_Codigo/Ejercicios_Taller4/src/ejercicios_taller4/Problema_3_Ejecutor.java
@@ -8,4 +8,51 @@ import java.util.Scanner;
  */
 public class Problema_3_Ejecutor {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Numero de estudiantes: ");
+        int cantidad = sc.nextInt();
+        sc.nextLine();
+
+        Problema_3_Autobus[] estudiantes = new Problema_3_Autobus[cantidad];
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("\nEstudiante: " + (i + 1));
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Grado: ");
+            String grado = sc.nextLine();
+
+            System.out.print("Permiso: \n1 = SI \n2 = NO: ");
+            int opcionPermiso = sc.nextInt();
+            sc.nextLine();
+
+            estudiantes[i] = new Problema_3_Autobus(nombre, grado, opcionPermiso);
+        }
+
+        int aceptados = 0;
+        int rechazados = 0;
+
+        System.out.println("\nVerificación de permiso");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println(estudiantes[i].toString());
+
+            if (estudiantes[i].permiso()) {
+                System.out.println("Permiso: Sí");
+                System.out.println("Puede usar el bus\n");
+                aceptados++;
+            } else {
+                System.out.println("Permiso: No");
+                System.out.println("No puede usar el bus\n");
+                rechazados++;
+            }
+        }
+
+        System.out.println("Permisos aceptados: " + aceptados);
+        System.out.println("Permisos rechazados: " + rechazados);
+
+        sc.close();
+    }
 }
